f4d523e693af6505dcda7e4ec6c58680e930eff2
Aditya Vij <vij.ad@northeastern.edu>
2025-12-28 20:59:13 -0800

    resumes upload

--- backend/controller/resume.controller.ts
+++ backend/controller/resume.controller.ts
@@ -8,37 +8,38 @@ import { getUploadUrl } from '../services/resume.service.ts';
  * @returns {express.Router} The router object containing the words routes.
  */
 const resumeController = () => {
-  const router = express.Router();
+    const router = express.Router();
 
-  const getUploadUrlRoute = async (req: ResumeMetadataRequest, res: Response) => {
-    const { file_name, file_type, file_size } = req.body;
+    const getUploadUrlRoute = async (req: ResumeMetadataRequest, res: Response) => {
+        const { file_name, file_type, file_size } = req.body;
 
-    if (!file_name || !file_type || !file_size) {
-      res.status(404).json({
-        "message": "Required arguments not found to post resume."
-      });
-      return;
-    }
+        if (!file_name || !file_type || !file_size) {
+            res.status(404).json({
+                "message": "Required arguments not found to post resume."
+            });
+            return;
+        }
 
-    try {
-      const url = await getUploadUrl(file_name, file_type, file_size);
+        try {
+            const url = await getUploadUrl(file_name, file_type, file_size);
+            console.log(url)
 
-      if ('error' in url) {
-        res.status(400).json({
-          "message": "Unable to post resume."
-        });
-        return;
-      }
-      res.status(200).json(url);
-    } catch (err: unknown) {
-      res.status(400).json({
-        "message": "Unable to post resume."
-      });
-    }
-  };
+            if ('error' in url) {
+                res.status(400).json({
+                    "message": "Unable to post resume."
+                });
+                return;
+            }
+            res.status(200).json(url);
+        } catch (err: unknown) {
+            res.status(400).json({
+                "message": "Unable to post resume."
+            });
+        }
+    };
 
-  router.post('/new', getUploadUrlRoute);
-  return router;
+    router.post('/new', getUploadUrlRoute);
+    return router;
 };
 
 export default resumeController;
