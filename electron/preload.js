e5e9132ff8e674db58b4fcd250b3e6291b8e0b5a
Aditya Vij <vij.ad@northeastern.edu>
2026-01-01 03:08:59 -0800

    introduce executing js

--- electron/preload.js
+++ electron/preload.js
@@ -3,6 +3,6 @@ const { contextBridge } = require('electron');
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('electronAPI', {
-  // Add your API methods here
+  // Add your API methods here if needed in the future
 });
 
